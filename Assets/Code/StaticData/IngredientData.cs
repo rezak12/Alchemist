c772e622de29649dd1c7708f5f2134fe63c11c09
Rezak <a.grek72@yahoo.com>
2023-09-01 11:49:43 +0300

    Add ingredients

---
+++ Assets/Code/StaticData/IngredientData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Code.Logic.Potions;
+using UnityEngine;
+
+namespace Code.StaticData
+{
+    [CreateAssetMenu(fileName = "IngredientData", menuName = "StaticData/Ingredient")]
+    public class IngredientData : ScriptableObject
+    {
+        [SerializeField] private List<IngredientCharacteristicAmountPair> _characteristics;
+
+        private List<PotionCharacteristicAmountPair> _convertedCharacteristics;
+        public List<PotionCharacteristicAmountPair> Characteristics
+        {
+            get
+            {
+                if (_convertedCharacteristics == null)
+                {
+                    ConvertInputToCharacteristicAmountPairList();
+                }
+
+                return _convertedCharacteristics;
+            }
+        }
+
+        private void ConvertInputToCharacteristicAmountPairList()
+        {
+            _convertedCharacteristics = _characteristics
+                .Select(characteristic =>
+                new PotionCharacteristicAmountPair(characteristic.Characteristic, characteristic.Amount))
+                .ToList();
+        }
+    }
+}
