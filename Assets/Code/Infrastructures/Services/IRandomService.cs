364cbf2e3299a3ddde9b1b873cdf8e378d5d7b08
Rezak <a.grek72@yahoo.com>
2023-09-02 11:51:12 +0300

    Create orders factory

---
+++ Assets/Code/Infrastructures/Services/IRandomService.cs
@@ -0,0 +1,8 @@
+﻿namespace Code.Infrastructures.Services
+{
+    public interface IRandomService
+    {
+        int Next(int min, int max);
+        float Next(float min, float max);
+    }
+}
---
+++ Assets/Code/Infrastructures/Services/PotionOrderFactory.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Code.Logic.Orders;
+using Code.Logic.Potions;
+using Code.StaticData;
+
+namespace Code.Infrastructures.Services
+{
+    public class PotionOrderFactory : IPotionOrderFactory
+    {
+        private readonly IRandomService _randomService;
+
+        public PotionOrderFactory(IRandomService randomService)
+        {
+            _randomService = randomService;
+        }
+
+        public PotionOrder CreateOrder(PotionOrderDifficulty orderDifficulty, PotionOrderType orderType)
+        {
+            var orderDifficultyName = orderDifficulty.Name;
+            var orderTypeName = orderType.Name;
+            OrderReward reward = CreateReward(orderDifficulty, orderType);
+            List<PotionCharacteristicAmountPair> requirementCharacteristics = 
+                CreateRequirementCharacteristics(orderDifficulty, orderType);
+
+            return new PotionOrder(orderDifficultyName, orderTypeName, requirementCharacteristics, reward);
+        }
+
+        private List<PotionCharacteristicAmountPair> CreateRequirementCharacteristics(PotionOrderDifficulty orderDifficulty, PotionOrderType orderType)
+        {
+            var characteristicsAmount = orderDifficulty.RequirementCharacteristicsAmount;
+            var characteristics = orderType.RequirementPotionCharacteristics.Take(characteristicsAmount);
+            
+            var result = new List<PotionCharacteristicAmountPair>(characteristicsAmount);
+            foreach (PotionCharacteristic characteristic in characteristics)
+            {
+                var characteristicPointsAmount = _randomService.Next(
+                    orderDifficulty.MinRequirementCharacteristicPoints,
+                    orderDifficulty.MaxRequirementCharacteristicPoints);
+
+                var characteristicAmountPair =
+                    new PotionCharacteristicAmountPair(characteristic, characteristicPointsAmount);
+                
+                result.Add(characteristicAmountPair);
+            }
+
+            return result;
+        }
+
+        private OrderReward CreateReward(PotionOrderDifficulty orderDifficulty, PotionOrderType orderType)
+        {
+            var coinsAmount = _randomService
+                .Next(orderDifficulty.MinCoinsAmountReward, orderDifficulty.MaxCoinsAmountReward);
+            
+            var reputationAmount = _randomService
+                .Next(orderDifficulty.MinReputationAmountReward, orderDifficulty.MaxReputationAmountReward);
+            
+            IngredientData ingredient;
+            if (_randomService.Next(0, 100) >= orderDifficulty.IngredientAsRewardChance)
+            {
+                ingredient = null;
+            }
+            else
+            {
+                ingredient = orderType
+                    .PossibleRewardIngredients[_randomService.Next(0, orderType.PossibleRewardIngredients.Count)];
+            }
+
+            return new OrderReward(coinsAmount, reputationAmount, ingredient);
+        }
+    }
+}
