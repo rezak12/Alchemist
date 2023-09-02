e4f4d0855d804229179adb444e1ecd75f8ea83ba
Rezak <a.grek72@yahoo.com>
2023-09-02 15:43:45 +0300

    Create potion factory

---
+++ Assets/Code/Logic/Potions/IPotionFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Code.StaticData;
+
+namespace Code.Logic.Potions
+{
+    public interface IPotionFactory
+    {
+        PotionInfo CreatePotion(IEnumerable<IngredientData> ingredients);
+    }
+}
