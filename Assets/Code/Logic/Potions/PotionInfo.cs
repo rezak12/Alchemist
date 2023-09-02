8a3a10923012e9b4a210f413c6b01c15c92071eb
Rezak <a.grek72@yahoo.com>
2023-09-02 15:43:25 +0300

    Create potion information class

---
+++ Assets/Code/Logic/Potions/PotionInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Code.Logic.Potions
+{
+    public class PotionInfo
+    {
+        public List<PotionCharacteristicAmountPair> Characteristics { get; }
+
+        public PotionInfo(List<PotionCharacteristicAmountPair> characteristics)
+        {
+            Characteristics = characteristics;
+        }
+    }
+}
