0275d906a1a4f6b03a5c579d9e7dcc4ec415b2a1
Rezak <a.grek72@yahoo.com>
2023-09-02 11:49:24 +0300

    Create order configuration data

---
+++ Assets/Code/StaticData/PotionOrderType.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Code.StaticData
+{
+    [CreateAssetMenu(fileName = "PotionOrderDifficultyData", menuName = "StaticData/PotionOrders/OrderType")]
+    public class PotionOrderType : ScriptableObject
+    {
+        [SerializeField] private string _name;
+        [SerializeField] private List<PotionCharacteristic> _requirementPotionCharacteristics;
+        [SerializeField] private List<IngredientData> _possibleRewardIngredients;
+
+        public string Name => _name;
+        public List<PotionCharacteristic> RequirementPotionCharacteristics => _requirementPotionCharacteristics;
+        public List<IngredientData> PossibleRewardIngredients => _possibleRewardIngredients;
+    }
+}
