4f33f87b7b42915a5cc9e4198d6b943c8fe0117f
Rezak <a.grek72@yahoo.com>
2023-09-04 13:35:58 +0300

    Create save/load progress system

---
+++ Assets/Code/Infrastructure/Services/ProgressServices/PlayerProgress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Code.Infrastructure.Services.ProgressServices
+{
+    [Serializable]
+    public class PlayerProgress
+    {
+        public int CoinsAmount;
+        public int ReputationAmount;
+        public List<string> PlayerIngredientsGUIDs;
+
+        public PlayerProgress(int coinsAmount, int reputationAmount, IEnumerable<string> playerIngredientsGUIDs)
+        {
+            CoinsAmount = coinsAmount;
+            ReputationAmount = reputationAmount;
+            PlayerIngredientsGUIDs = new List<string>(playerIngredientsGUIDs);
+        }
+    }
+}
