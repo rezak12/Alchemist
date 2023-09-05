4d9f25cc1656b5464d1bb248ad96a63b61591800
Rezak <a.grek72@yahoo.com>
2023-09-05 14:06:47 +0300

    Create UI factory

---
+++ Assets/Code/Infrastructure/Services/Factories/IUIFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Code.Logic.PotionMaking;
+using Code.StaticData;
+using Code.UI.PlayerIngredientUI;
+using UnityEngine;
+
+namespace Code.Infrastructure.Services.Factories
+{
+    public interface IUIFactory
+    {
+        Task<PlayerIngredientsPanel> CreatePlayerIngredientsPanelAsync(AlchemyTable alchemyTable);
+        Task<IngredientItemUI> CreateIngredientItemUIAsync(IngredientData ingredient, AlchemyTable alchemyTable, Transform parent);
+        Task<IngredientCharacteristicItemUI> CreateIngredientCharacteristicItemUIAsync(
+            IngredientCharacteristicAmountPair characteristicAmountPair,
+            Transform parent);
+    }
+}
