6431afca59986bca87c255d13bf99b5faab6052e
Rezak <a.grek72@yahoo.com>
2023-09-05 14:05:10 +0300

    Add WindowConfig ScriptableObject

---
+++ Assets/Code/StaticData/WindowConfig.cs
@@ -0,0 +1,16 @@
+﻿using Code.UI;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Code.StaticData
+{
+    [CreateAssetMenu(fileName = "WindowConfig", menuName = "StaticData/Windows/WindowConfig")]
+    public class WindowConfig : ScriptableObject
+    {
+        [SerializeField] private WindowType _type;
+        [SerializeField] private AssetReferenceGameObject _prefabReference;
+
+        public WindowType Type => _type;
+        public AssetReferenceGameObject PrefabReference => _prefabReference;
+    }
+}
