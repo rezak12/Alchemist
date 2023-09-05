012620055b7e97db557490c972d79c193e1ad790
Rezak <a.grek72@yahoo.com>
2023-09-05 13:59:40 +0300

    Add icons to potion characteristic data

--- Assets/Code/StaticData/PotionCharacteristic.cs
+++ Assets/Code/StaticData/PotionCharacteristic.cs
@@ -6,7 +6,9 @@ namespace Code.StaticData
     public class PotionCharacteristic : ScriptableObject
     {
         [SerializeField] private string _name;
+        [SerializeField] private Sprite _icon;
         
         public string Name => _name;
+        public Sprite Icon => _icon;
     }
 }
