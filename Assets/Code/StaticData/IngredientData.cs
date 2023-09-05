da16e2efdfa0215a75153d0e6be226fd8e2ce120
Rezak <a.grek72@yahoo.com>
2023-09-05 14:01:00 +0300

    Add name and icon to ingredient data

--- Assets/Code/StaticData/IngredientData.cs
+++ Assets/Code/StaticData/IngredientData.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Code.StaticData
 {
     [CreateAssetMenu(fileName = "IngredientData", menuName = "StaticData/Ingredient")]
     public class IngredientData : ScriptableObject
     {
-        [SerializeField] private List<IngredientCharacteristicAmountPair> _characteristics;
-        public IEnumerable<IngredientCharacteristicAmountPair> Characteristics => _characteristics;
+        [SerializeField] private Sprite _icon;
+        [SerializeField] private string _name;
+        [FormerlySerializedAs("_characteristics")] 
+        [SerializeField] private List<IngredientCharacteristicAmountPair> _characteristicAmountPairs;
+
+        public Sprite Icon => _icon;
+        public string Name => _name;
+        public IEnumerable<IngredientCharacteristicAmountPair> CharacteristicAmountPairs => _characteristicAmountPairs;
     }
 }
