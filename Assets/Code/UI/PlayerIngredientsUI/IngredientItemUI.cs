a47aade0f83dc70cbe316de7bd38099d8722dad6
Rezak <a.grek72@yahoo.com>
2023-09-10 19:02:50 +0300

    Create ingredient item UI and build PlayerIngredientsPanel prefab

--- Assets/Code/UI/PlayerIngredientsUI/IngredientItemUI.cs
+++ Assets/Code/UI/PlayerIngredientsUI/IngredientItemUI.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Code.Infrastructure.Services.Factories;
 using Code.Logic.PotionMaking;
 using Code.StaticData;
+using Code.UI.PotionCharacteristicsUI;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,7 +14,7 @@ namespace Code.UI.PlayerIngredientsUI
     {
         [SerializeField] private TextMeshProUGUI _nameText;
         [SerializeField] private Image _iconImage;
-        [SerializeField] private Transform _characteristicInfoItemsContainer;
+        [SerializeField] private PotionCharacteristicItemsContainer _characteristicItemsContainer;
         [SerializeField] private Button _useButton;
         
         private IngredientData _ingredient;
@@ -27,7 +28,9 @@ namespace Code.UI.PlayerIngredientsUI
             _nameText.text = ingredient.Name;
             _iconImage.sprite = ingredient.Icon;
             
-            await CreateIngredientCharacteristicItemsAsync(ingredient, uiFactory);
+            await _characteristicItemsContainer.CreateCharacteristicItemsAsync(
+                ingredient.CharacteristicAmountPairs, 
+                uiFactory);
 
             _alchemyTable.FilledSlotsAmountChanged += FilledSlotAmountChanged;
             _useButton.onClick.AddListener(UseIngredient);
@@ -37,19 +40,6 @@ namespace Code.UI.PlayerIngredientsUI
         {
             _alchemyTable.FilledSlotsAmountChanged -= FilledSlotAmountChanged;
             _useButton.onClick.RemoveListener(UseIngredient);
-        }
-
-        private async Task CreateIngredientCharacteristicItemsAsync(IngredientData ingredient, IUIFactory uiFactory)
-        {
-            var tasks = new List<Task>(ingredient.CharacteristicAmountPairs.Count);
-            foreach (IngredientCharacteristicAmountPair characteristicAmountPair in ingredient.CharacteristicAmountPairs)
-            {
-                Task task = uiFactory.CreateIngredientCharacteristicItemUIAsync(
-                    characteristicAmountPair, _characteristicInfoItemsContainer);
-                tasks.Add(task);
-            }
-
-            await Task.WhenAll(tasks);
         }
 
         private void FilledSlotAmountChanged()
