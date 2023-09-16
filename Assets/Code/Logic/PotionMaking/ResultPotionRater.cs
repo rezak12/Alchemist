e48adcf3d8b3489ee0fb604c41072d50336ac10a
Rezak <a.grek72@yahoo.com>
2023-09-16 19:08:58 +0300

    Create ResultPotionRater

---
+++ Assets/Code/Logic/PotionMaking/ResultPotionRater.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using Code.Infrastructure.Services.Factories;
+using Code.Logic.Orders;
+using Code.Logic.Potions;
+
+namespace Code.Logic.PotionMaking
+{
+    public class ResultPotionRater
+    {
+        public bool RatePotionByOrderRequirementCharacteristics(PotionOrder order, Potion potion)
+        {
+            var isPotionMatchesRequirementCharacteristics = IsPotionMatchesRequirementCharacteristics(order, potion);
+            return isPotionMatchesRequirementCharacteristics;
+        }
+
+        private bool IsPotionMatchesRequirementCharacteristics(PotionOrder order, Potion potion)
+        {
+            var requirementCharacteristicAmountPairs = order
+                .RequirementCharacteristics
+                .ToDictionary(pair => pair.Characteristic, pair => pair.PointsAmount);
+            
+            var takenCharacteristicAmountPairs = potion
+                .CharacteristicAmountPairs
+                .ToDictionary(pair => pair.Characteristic, pair => pair.PointsAmount);
+
+            
+            foreach (var characteristicAmountPair in requirementCharacteristicAmountPairs)
+            {
+                if (!takenCharacteristicAmountPairs.TryGetValue(characteristicAmountPair.Key, out var amount))
+                {
+                    return false;
+                }
+                else if (amount < characteristicAmountPair.Value)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
