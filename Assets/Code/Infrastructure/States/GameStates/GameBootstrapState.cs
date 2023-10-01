fb4566b1d187cdad5577589631214a3a3c57cda4
Rezak <a.grek72@yahoo.com>
2023-10-01 17:25:04 +0300

    Create GameBootstrapState

---
+++ Assets/Code/Infrastructure/States/GameStates/GameBootstrapState.cs
@@ -0,0 +1,37 @@
+﻿using Code.Infrastructure.Services.AssetProvider;
+using Code.Infrastructure.Services.StaticData;
+using Cysharp.Threading.Tasks;
+
+namespace Code.Infrastructure.States.GameStates
+{
+    public class GameBootstrapState : IState
+    {
+        private readonly IAssetProvider _assetProvider;
+        private readonly IStaticDataService _staticDataService;
+        private readonly GameStateMachine _stateMachine;
+
+        public GameBootstrapState(
+            IAssetProvider assetProvider, 
+            IStaticDataService staticDataService, 
+            GameStateMachine stateMachine)
+        {
+            _assetProvider = assetProvider;
+            _staticDataService = staticDataService;
+            _stateMachine = stateMachine;
+        }
+
+        public async UniTask Enter()
+        {
+            await UniTask.WhenAll(
+                _assetProvider.InitializeAsync(), 
+                _staticDataService.InitializeAsync());
+            
+            
+        }
+
+        public UniTask Exit()
+        {
+            return default;
+        }
+    }
+}
