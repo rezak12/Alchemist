c4408de7f3e9ee60e3e9056daf6f8c139afb25d4
Rezak <a.grek72@yahoo.com>
2023-10-03 14:28:11 +0300

    Create states factory

---
+++ Assets/Code/Infrastructure/Services/Factories/IStatesFactory.cs
@@ -0,0 +1,9 @@
+﻿using Code.Infrastructure.States;
+
+namespace Code.Infrastructure.Services.Factories
+{
+    public interface IStatesFactory
+    {
+        public TState Create<TState>() where TState : IExitableState;
+    }
+}
