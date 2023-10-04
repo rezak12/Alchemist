f5202f00e4f69e18206c47d0b2c27ef0eb3df0b0
Rezak <a.grek72@yahoo.com>
2023-10-04 19:19:33 +0300

    IngredientAnimator refactoring

--- Assets/Code/Animations/IngredientAnimator.cs
+++ Assets/Code/Animations/IngredientAnimator.cs
@@ -2,22 +2,23 @@
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Code.Animations
 {
     public class IngredientAnimator : MonoBehaviour
     {
-        [SerializeField] private float _removeDurationInSeconds = 10f;
+        [SerializeField] private float _moveDurationInSeconds = 1f;
 
         public async UniTaskVoid MoveToSlot(Transform slotTransform)
         {
-            await transform.DOMove(slotTransform.position, 10f, false)
+            await transform.DOMove(slotTransform.position, _moveDurationInSeconds, false)
                 .WithCancellation(this.GetCancellationTokenOnDestroy());
         }
 
         public async UniTaskVoid RemoveFromSlotThenDestroy(Transform to, Action onRemoved = null)
         {
-            await transform.DOJump(to.position, 5f, 1, _removeDurationInSeconds, false)
+            await transform.DOJump(to.position, 5f, 1, _moveDurationInSeconds, false)
                 .WithCancellation(this.GetCancellationTokenOnDestroy());
             
             onRemoved?.Invoke();
