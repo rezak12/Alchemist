c1ee98738776eda50e606f2395ac7d8943ef2f16
Rezak <a.grek72@yahoo.com>
2023-10-27 15:39:35 +0300

    Add VFX prefabs

---
+++ Assets/Code/Infrastructure/Services/VFX/VFX.cs
@@ -0,0 +1,12 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using Zenject;
+
+namespace Code.Infrastructure.Services.FX
+{
+    public class VFX : MonoBehaviour
+    {
+        public class Factory : PlaceholderFactory<AssetReferenceGameObject, UniTask<VFX>> { }
+    }
+}
