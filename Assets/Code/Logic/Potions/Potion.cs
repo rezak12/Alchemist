27d5b6616d214acd6e2b0b5989fd46e4ab60b7a3
Rezak <a.grek72@yahoo.com>
2023-11-27 14:54:40 +0200

    Add SFX reference to Potion class

--- Assets/Code/Logic/Potions/Potion.cs
+++ Assets/Code/Logic/Potions/Potion.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace Code.Logic.Potions
 {
@@ -7,12 +8,14 @@ namespace Code.Logic.Potions
     {
         public IEnumerable<PotionCharacteristicAmountPair> CharacteristicAmountPairs =>
             _potionInfo.CharacteristicsAmountPairs;
+        public AssetReferenceT<AudioClip> SFXReference { get; private set; }
         
         private PotionInfo _potionInfo;
 
-        public void Initialize(PotionInfo potionInfo)
+        public void Initialize(PotionInfo potionInfo, AssetReferenceT<AudioClip> sfxReference)
         {
             _potionInfo = potionInfo;
+            SFXReference = sfxReference;
         }
     }
 }
