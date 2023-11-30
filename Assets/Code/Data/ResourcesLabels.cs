623dc3d64ab934b800dea2c5fec8b339945f6a02
Rezak <a.grek72@yahoo.com>
2023-11-30 14:31:58 +0200

    Give labels to addressables

---
+++ Assets/Code/Data/ResourcesLabels.cs
@@ -0,0 +1,9 @@
+﻿namespace Code.Data
+{
+    public class ResourcesLabels
+    {
+        public const string PotionMaking = "PotionMaking";
+        public const string PotionCharacteristic = "PotionCharacteristic";
+        public const string PotionIngredient = "PotionIngredient";
+    }
+}
