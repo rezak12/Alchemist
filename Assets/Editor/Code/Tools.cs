7c950c9eaf6cfcdc7bcb5102ad35bee68810c000
Rezak <a.grek72@yahoo.com>
2023-12-07 12:08:44 +0200

    Add opportunity to delete save in editor

--- Assets/Editor/Code/Tools.cs
+++ Assets/Editor/Code/Tools.cs
@@ -1,5 +1,7 @@
-﻿using UnityEditor;
+﻿using Code.Data;
+using UnityEditor;
 using UnityEngine;
+using File = System.IO.File;
 
 namespace Editor.Code
 {
@@ -10,6 +12,14 @@ namespace Editor.Code
         {
             PlayerPrefs.DeleteAll();
             PlayerPrefs.Save();
+        }
+
+        [MenuItem("Tools/DeleteSaveFile")]
+        public static void DeleteSaveFile()
+        {
+            var path = $"{Application.persistentDataPath}/{Constants.SaveFileName}";
+            if(!File.Exists(path)) return;
+            File.Delete(path);
         }
     }
 }
