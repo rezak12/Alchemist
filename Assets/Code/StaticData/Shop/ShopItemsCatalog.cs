bf08ca670bf3861dd15464cb06f74f6bebfe072b
Rezak <a.grek72@yahoo.com>
2024-01-11 13:42:02 +0200

    Replace references in ShopItemsCatalog y actual items

--- Assets/Code/StaticData/Shop/ShopItemsCatalog.cs
+++ Assets/Code/StaticData/Shop/ShopItemsCatalog.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 
 namespace Code.StaticData.Shop
 {
     [CreateAssetMenu(fileName = "ShopItemsCatalog", menuName = "StaticData/Shop/Catalog")]
     public class ShopItemsCatalog : ScriptableObject
     {
-        [SerializeField] private List<AssetReferenceT<PotionShopItem>> _potionItems;
-        [SerializeField] private List<AssetReferenceT<TableShopItem>> _tableItems;
-        [SerializeField] private List<AssetReferenceT<EnvironmentShopItem>> _environmentItems;
+        [SerializeField] private List<PotionShopItem> _potionItems;
+        [SerializeField] private List<TableShopItem> _tableItems;
+        [SerializeField] private List<EnvironmentShopItem> _environmentItems;
 
-        public IReadOnlyCollection<AssetReferenceT<PotionShopItem>> PotionItems => _potionItems;
-        public IReadOnlyCollection<AssetReferenceT<TableShopItem>> TableItems => _tableItems;
-        public IReadOnlyCollection<AssetReferenceT<EnvironmentShopItem>> EnvironmentItems => _environmentItems;
+        public IReadOnlyCollection<PotionShopItem> PotionItems => _potionItems;
+        public IReadOnlyCollection<TableShopItem> TableItems => _tableItems;
+        public IReadOnlyCollection<EnvironmentShopItem> EnvironmentItems => _environmentItems;
     }
 }
